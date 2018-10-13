c415a19d99a4b70143bf3b1d26aeba66f7c13d9e
Pratik Savla <pratiksavla07@gmail.com>
2018-10-13 21:56:31 +0530

    Update and rename manage.py to hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -1,7 +1,5 @@
-import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
 
+#get_ipython().run_line_magic('matplotlib', 'inline')
 import warnings
 warnings.filterwarnings("ignore")
 import os, argparse
@@ -13,10 +11,21 @@ from sklearn.externals import joblib
 from keras import backend as K
 from keras.utils.vis_utils import plot_model
 K.set_image_data_format('channels_first')
+#K.set_image_dim_ordering('th')
 
+
+# In[2]:
+
+
+# File paths for the model, all of these except the CNN Weights are 
+# provided in the repo, See the models/CNN/README.md to download VGG weights
 VQA_model_file_name      = 'VQA/VQA_MODEL.json'
 VQA_weights_file_name   = 'VQA/VQA_MODEL_WEIGHTS.hdf5'
 label_encoder_file_name  = 'VQA/FULL_labelencoder_trainval.pkl'
+
+
+# In[3]:
+
 
 def get_image_model():
     from keras.applications.vgg16 import VGG16
@@ -31,8 +40,16 @@ def get_image_model():
     model_new.compile(optimizer=sgd, loss='categorical_crossentropy')
     return model_new
 
+
+# In[4]:
+
+
 model_vgg = get_image_model()
-print("Vgg model imported")
+#model_vgg.summary()
+
+
+# In[5]:
+
 
 def get_image_features(image_file_name):
     ''' Runs the given image_file to VGG 16 model and returns the 
@@ -54,6 +71,10 @@ def get_image_features(image_file_name):
     image_features[0,:] = model_vgg.predict(im)[0]
     return image_features
 
+
+# In[6]:
+
+
 def get_question_features(question):
     ''' For a given question, a unicode string, returns the time series vector
     with each word (token) transformed into a 300 dimension representation
@@ -66,6 +87,16 @@ def get_question_features(question):
     return question_tensor
 
 
+# In[7]:
+
+
+##If there is any error with "en_vectors_web_lg" run the following:
+#!python -m spacy download en_vectors_web_lg
+
+
+# In[8]:
+
+
 def get_VQA_model(VQA_model_file_name, VQA_weights_file_name):
     vqa_model = model_from_json(open(VQA_model_file_name).read())
     vqa_model.load_weights(VQA_weights_file_name)
@@ -73,59 +104,51 @@ def get_VQA_model(VQA_model_file_name, VQA_weights_file_name):
     return vqa_model
 
 
+# In[9]:
+
+
 model_vqa = get_VQA_model(VQA_model_file_name, VQA_weights_file_name)
-print("vqa model loaded")
-question = u"What vehicle is in the picture?"
+#model_vqa.summary()
+
+labelencoder = joblib.load(label_encoder_file_name)
+# In[10]:
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
 
 
-UPLOAD_FOLDER = 'folders'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+from flask import request
+from flask import jsonify
+from flask import Flask
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-        	#question1 = request.files['text']
-        	#print(question1)
-        	filename = secure_filename(file.filename)
-        	file.save(filename)
-        	question = filename.split(".")
-        	question = question[0].split("_")
-        	question = ' '.join(quest for quest in question)
-        	question = question + "?"
-        	print(question)
-        	image_features = get_image_features(filename)
-        	question_features = get_question_features(question)
-        	y_output = model_vqa.predict([question_features, image_features])
-        	warnings.filterwarnings("ignore", category=DeprecationWarning)
-        	labelencoder = joblib.load(label_encoder_file_name)
-        	for label in reversed(np.argsort(y_output)[0,-5:]):
-        		print(str(round(y_output[0,label]*100,2)).zfill(5), "% ", labelencoder.inverse_transform(label))
-        		return redirect(url_for('upload_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+@app.route('/hello', methods=['POST'])
+def hello():
+    message = request.get_json(force=True)
+    question = message['name']
+    
+    while(True):
+    	ret, frame = cap.read()
+    	#question = u'what is there in the picture?'
+    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    	cv2.imshow('frame',gray)
+    	if cv2.waitKey(1) & 0xFF == ord('c'):
+	    	image_features = np.zeros((1, 4096))
+	    	im = cv2.resize(frame, (224, 224))
+	    	im = im.transpose((2,0,1))
+	    	im = np.expand_dims(im, axis=0) 
+	    	image_features[0,:] = model_vgg.predict(im)[0]
+	    	question_features = get_question_features(question)
+	    	y_output = model_vqa.predict([question_features, image_features])
+	    	warnings.filterwarnings("ignore", category=DeprecationWarning)
+	    	labelencoder = joblib.load(label_encoder_file_name)
+	    	for label in reversed(np.argsort(y_output)[0,-5:]):
+	    		ans =  labelencoder.inverse_transform(label)
+	    		break
+	    	response = {'greeting':'The answer is '+ ans + '!'}
+	    	break
+    #cap.release()
+    cv2.destroyAllWindows()
+    return jsonify(response)
